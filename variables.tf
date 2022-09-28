2678563026c0c98ce5a160f114c5357dc06cc60b
James Bond <neonhustle@outlook.com>
2022-09-28 09:47:39 -0500

    TF Module Files Second Commit

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "location" {
   type        = string
 }
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "The name of the resource group."
   type        = string
 }
 variable "storage_account_name" {
-  description = "The name of the storage account"
+  description = "The name of the storage account."
   type        = string
 }
 variable "storage_account_tier" {
